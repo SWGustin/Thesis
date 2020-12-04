27281d409d40416086374615808401898c4f096d
SWGustin <sam@praxispd.com>
2020-12-03 20:34:14 -0800

    clean and annotate viewer

--- software/playground.py
+++ software/playground.py
@@ -8,40 +8,45 @@ import MVC.Model.object_model as model
 config_file_name = 'config.json'
 config_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '\\MVC\\View\\' + config_file_name
 
+##this gets the corners ofthe wing
+#def corners
 with open(config_path, 'r') as f:
     reader = json.load(f)
 
 geometry = reader['wing_geometry']    
 print(geometry)
-corners = [(0,0)]
+corners = [(0,geometry['root_chord'])]
 tip_setback = -int(geometry['span'] / 2 * np.sin(np.radians(geometry['sweep_angle'])))
-corners.append([geometry['span'] / 2, tip_setback])
-corners.append([geometry['span'] / 2, tip_setback-geometry['tip_chord']])
-corners.append([0,-geometry['root_chord']])
+corners.append([geometry['span'] / 2, geometry['root_chord'] + tip_setback])
+corners.append([geometry['span'] / 2, geometry['root_chord'] + tip_setback - geometry['tip_chord']])
+corners.append([0,0])
 
 print(corners)
+#end corners func
 
+
+
+##make an arpel
 apel = model.ArPel('config.json')
 
 coord = corners
-coord.append(coord[0]) #repeat the first point to create a 'closed loop'
-
 xs, ys = zip(*coord) #create lists of x and y values
 
-plt.figure()
+
+#setup canvas
+fig, ax = plt.subplots(1,1)
+#put shape on subplot
 plt.plot(xs,ys) 
-plt.show() # if you need...
-
-
-X, Y = np.mgrid[:2*np.pi:10j,:2*np.pi:5j]
+#do fancy quiver arrows shit
+#controls placement of arrow roots
+X, Y = np.mgrid[:20*np.pi:10j,:6*np.pi:5j]
 U = np.cos(X)
 V = np.sin(Y)
 
-fig, ax = plt.subplots(1,1)
 Q = ax.quiver(X, Y, U, V, pivot='mid', color='r', units='inches')
 
-ax.set_xlim(-1, 7)
-ax.set_ylim(-1, 7)
+ax.set_xlim(-1, geometry['span'] / 2 + 2)
+ax.set_ylim(-1, geometry['root_chord'] + 2)
 
 def update_quiver(num, Q, X, Y):
     """updates the horizontal and vertical vector components by a
